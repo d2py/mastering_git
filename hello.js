06338077dc8de4e0c775f6cfc8b272f27c9f3ae8
Diego <ddias.py@gmail.com>
2025-05-20 09:31:01 -0300

    save the day

--- hello.js
+++ hello.js
@@ -1,11 +1,5 @@
 console.log('Hello, Git!')
-<<<<<<< HEAD
-<<<<<<< HEAD
 console.log('Hello, GitHub!')
 console.log('TRYING OUT REVERT!')
-=======
 
->>>>>>> parent of 0c14aa6 (add github console.log to hello.js)
-=======
-
->>>>>>> parent of 0c14aa6 (add github console.log to hello.js)
+// IMPLEMENT URGENT FIX
