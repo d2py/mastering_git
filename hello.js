2624a9bab5a502c16f7ecd9b86da1677241d1e9e
Diego <ddias.py@gmail.com>
2025-05-20 09:10:13 -0300

    Revert "add github console.log to hello.js"
    This reverts commit 0c14aa671c8b049874a0eeb5579843d56a0c0046.
    
    ~
    :qa!

--- hello.js
+++ hello.js
@@ -1,7 +1,11 @@
 console.log('Hello, Git!')
 <<<<<<< HEAD
+<<<<<<< HEAD
 console.log('Hello, GitHub!')
 console.log('TRYING OUT REVERT!')
+=======
+
+>>>>>>> parent of 0c14aa6 (add github console.log to hello.js)
 =======
 
 >>>>>>> parent of 0c14aa6 (add github console.log to hello.js)
